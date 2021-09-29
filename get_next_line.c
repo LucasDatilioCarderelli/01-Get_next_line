057e06fe9d854b32f53a278df33207745abaf3b3
LucasDatilioCarderelli <llucascarderelli@gmail.com>
2021-09-29 19:51:04 -0300

    working well

--- get_next_line.c
+++ get_next_line.c
@@ -6,43 +6,99 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 16:37:17 by ldatilio          #+#    #+#             */
-/*   Updated: 2021/09/23 16:37:12 by ldatilio         ###   ########.fr       */
+/*   Updated: 2021/09/29 19:48:27 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-BUFFER_SIZE = 42;
-
-char	*make_backup(fd, backup, buffer)
+static char	*make_backup(int fd, char *backup, char	*buffer)
 {
-	if (read(fd, buffer, BUFFER_SIZE) == -1)
-		return (NULL);
+	int		bytes;
+	int		control;
+
+	bytes = 1;
+	control = 0;
+	while (control == 0 && bytes != 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == -1 )
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[bytes] = '\0';
+		if (!backup)
+			backup = "";
+		backup = ft_strjoin(backup, buffer);
+		if (ft_strchr(backup, '\n'))
+			control = 1;
+	}
+	free(buffer);
 	return (backup);
+}
+
+static char	*make_line(char *backup)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (backup[i] && backup[i - 1] != '\n')
+		i++;
+	line = malloc(sizeof(char *) * (i + 1));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, backup, i + 1);
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
+}
+
+static char	*make_newbackup(char *backup)
+{
+	int		i;
+	int		lenbackup;
+	char	*newbackup;
+
+	i = 0;
+	while (backup[i] && backup[i] != '\n')
+		i++;
+	if (backup[i] == '\0')
+	{
+		free(backup);
+		return (NULL);
+	}
+	lenbackup = ft_strlen(backup);
+	newbackup = (char *)malloc(sizeof(char) * (lenbackup - i + 1));
+	if (!newbackup)
+		return (NULL);
+	ft_strlcpy(newbackup, backup + i + 1, lenbackup - i + 1);
+	free(backup);
+	return (newbackup);
 }
 
 char	*get_next_line(int fd)
 {
-	char		*buffer;
 	char		*line;
+	char		*buffer;
 	static char	*backup;
 
-	if (!BUFFER_SIZE || !fd)
-		return (0);
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buffer)
+	{
+		free(buffer);
+		return (NULL);
+	}
 	backup = make_backup(fd, backup, buffer);
+	if (!backup)
+		return (NULL);
 	line = make_line(backup);
+	backup = make_newbackup(backup);
 	return (line);
-}
-
-int	main(void)
-{
-	FILE	*file;
-
-	file = open("test.txt", "r");
-	printf("%s", get_next_line(file));
-	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,18 @@
 /*   By: ldatilio <ldatilio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 16:37:19 by ldatilio          #+#    #+#             */
-/*   Updated: 2021/09/23 14:34:46 by ldatilio         ###   ########.fr       */
+/*   Updated: 2021/09/29 19:48:48 by ldatilio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# define MAX_FD 256
+
 # include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
 
 /**	@brief	Description Write a function which returns a line read from a
  *			file descriptor.
@@ -21,5 +25,34 @@
  *	@return	Read line: correct behavior. NULL if there is nothing else to read
  *			or an error occurred */
 char	*get_next_line(int fd);
+
+/**	@brief	calculates the length of the string pointed to by s,
+ * 			excluding the terminating null byte ('\0').
+ *	@param 	s Pointer to the initial area of the string
+ *	@return	size_t Returns the number of bytes in the string pointed to by s.*/
+size_t	ft_strlen(const char *s);
+
+/**	@brief	Allocates (with malloc(3)) and returns a newstring,
+ *			which is the result of the concatenation of ’s1’ and ’s2’.
+ *	@param	s1 The prefix string
+ *	@param	s2 The suffix string
+ *	@return char* Returns the new string or NULL if the allocation fails. */
+char	*ft_strjoin(char const *s1, char const *s2);
+
+/** @brief	Returns a pointer to the first occurrence
+ * 			of the character c in the string s.
+ * @param	s Pointer to the string memory area
+ * @param	c Character to be searched inside the string s
+ * @return	char* Return a pointer to the matched character
+ * 			or NULL if the character is not found. */
+char	*ft_strchr(const char *s, int c);
+
+/**	@brief	Copies up to size - 1 characters from
+ * 			the NUL-terminated string src to dest, NUL-terminating the result.
+ * @param	dest	Pointer to the destination area of memory
+ * @param	src		Pointer to the source area of memory
+ * @param	size	Buffer size in bytes
+ * @return	size_t Returns the length of src. */
+size_t	ft_strlcpy(char *dest, const char *src, size_t destsize);
 
 #endif
